158ba36b5183cf412fc944b7eac5a0498c0108f9
Manojseetaram <manojseetaram.artytech@gmail.com>
2026-02-13 22:40:37 +0530

    UPDATED A FAVIICON

---
+++ src/app/pages/_document.js
@@ -0,0 +1,16 @@
+import { Html, Head, Main, NextScript } from 'next/document';
+
+export default function Document() {
+  return (
+    <Html>
+      <Head>
+        {/* This is your favicon */}
+        <link rel="icon" href="/favicon.ico" />
+      </Head>
+      <body>
+        <Main />
+        <NextScript />
+      </body>
+    </Html>
+  );
+}
