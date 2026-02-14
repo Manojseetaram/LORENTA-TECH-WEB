8f6ad4af245d074ef5e267db4c6a0da468febef4
Manojseetaram <manojseetaram.artytech@gmail.com>
2026-02-14 11:21:25 +0530

    updated faviicon

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -5,13 +5,18 @@ import SmoothScroll from "./smooth-scroll";
 export const metadata: Metadata = {
   title: "Lorenta Technologies",
   description: "Tech-enabled smart document printing kiosks",
+  icons: {
+    icon: "/favicon.ico",
+    apple: "/apple-touch-icon.png",
+  },
+  manifest: "/site.webmanifest",
 };
 
 export default function RootLayout({
   children,
-}: Readonly<{
+}: {
   children: React.ReactNode;
-}>) {
+}) {
   return (
     <html lang="en">
       <body>
